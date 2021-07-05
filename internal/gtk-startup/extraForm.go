2730ef5b3c232a0d69a4171d6327ca5023dc086f
Per Hultqvist <per@softteam.se>
2021-07-05 10:40:00 +0200

    renamed variable

--- internal/gtk-startup/extraForm.go
+++ internal/gtk-startup/extraForm.go
@@ -17,24 +17,26 @@ func (e *ExtraForm) OpenForm() {
 	builder := newSoftBuilder("main.glade")
 
 	// Get the extra window from glade
-	extraForm := builder.getObject("extra_window").(*gtk.Window)
+	extraWindow := builder.getObject("extra_window").(*gtk.Window)
 
 	// Set up the extra window
-	extraForm.SetTitle("extra form")
-	extraForm.HideOnDelete()
-	extraForm.SetModal(true)
-	extraForm.SetKeepAbove(true)
-	extraForm.SetPosition(gtk.WIN_POS_CENTER_ALWAYS)
+	extraWindow.SetTitle("extra form")
+	extraWindow.HideOnDelete()
+	extraWindow.SetModal(true)
+	extraWindow.SetKeepAbove(true)
+	extraWindow.SetPosition(gtk.WIN_POS_CENTER_ALWAYS)
 
 	// Hook up the destroy event
-	_, err := extraForm.Connect("destroy", extraForm.Destroy)
-	ErrorCheckWithPanic(err, "Failed to connect the extraForm.destroy event")
+	_, err := extraWindow.Connect("destroy", extraWindow.Destroy)
+	ErrorCheckWithPanic(err, "Failed to connect the extraWindow.destroy event")
 
 	// Close button
 	button := builder.getObject("extra_window_close_button").(*gtk.Button)
-	_, err = button.Connect("clicked", extraForm.Destroy)
+	_, err = button.Connect("clicked", extraWindow.Destroy)
 	ErrorCheckWithPanic(err, "Failed to connect the extra_window_close_button.clicked event")
 
+	e.Window = extraWindow
+
 	// Show the window
-	extraForm.ShowAll()
+	extraWindow.ShowAll()
 }
